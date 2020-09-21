238c5098af5ed3c5d0a9829914099b9c03b1e54f
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-09-22 01:09:56 +0530

    initial commit

---
+++ java_examples/my_First_Java_Project/src/mypack/MyFirstJavaClass.java
@@ -0,0 +1,20 @@
+package mypack;
+
+//class name and file name should be same
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+
+	}
+/*
+ * compile a java file using
+ * javac Filename.java
+ * and run it using
+ * java ClassName
+ * 
+ * and if you have to place your java class in a package then run below command
+ * javac -d . FileName.java
+ * java packagename.ClassName
+ */
+}
---
+++ java_examples/test_java_cmd_line/HelloWorld.java
@@ -0,0 +1,7 @@
+package hellopack;
+public class HelloWorld{
+
+public static void main(String a[]){
+System.out.println("Hello world from Cmd line");
+}
+}
